2e7ef5ae9f7cf256ac3ea26996fedddb7e11e912
Joseph Earl <jearl@thoughtworks.com>
2020-08-27 13:28:01 +0100

    Tidy up test

--- test/cases/simple.test.js
+++ test/cases/simple.test.js
@@ -1,21 +1,20 @@
 function terminalLog(violations) {
-    cy.task('log', `${violations.length} accessibility violation${
-        violations.length === 1 ? '' : 's'
-    } ${violations.length === 1 ? 'was' : 'were'} detected`)
-    if (violations.length > 0) {
-      // Pluck specific keys to keep the table readable
-      const violationData = violations.map(
-        ({ id, impact, description, nodes }) => ({
-          id,
-          impact,
-          description,
-          nodes: nodes.length
-        })
-      )
-      cy.task('table', violationData)
-    }
+  cy.task('log', `${violations.length} accessibility violation${
+    violations.length === 1 ? '' : 's'
+  } ${violations.length === 1 ? 'was' : 'were'} detected`)
+  if (violations.length > 0) {
+    // Pluck specific keys to keep the table readable
+    const violationData = violations.map(
+      ({ id, impact, description, nodes }) => ({
+        id,
+        impact,
+        description,
+        nodes: nodes.length
+      })
+    )
+    cy.task('table', violationData)
   }
-
+}
 
 describe('Simple', () => {
     it('finds accessibility issues', () => {
